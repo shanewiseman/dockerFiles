7f48e6773f9754745b07079a9c7b6111733f0252
Shane Wiseman <shane.db.wiseman@gmail.com>
2018-12-30 14:29:44 -0500

    made changes related to version of ubuntu referenced

--- cloud9/containerStart.sh
+++ cloud9/containerStart.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade -y 
-apt-get install build-essential libssl-dev git npm nodejs python curl vim python-pip -y
+apt-get install build-essential libssl-dev git npm nodejs python wget curl vim python-pip -y
 pip install cqlsh
 ln /usr/bin/nodejs /usr/bin/node
+
+#install go-lang
+wget https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf  go1.9.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+
 
 mkdir /var/www; cd /var/www/
 git clone https://github.com/c9/core.git
